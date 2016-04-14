12d20036235656e000b1832e9450fda4af3c6445
François LEPORCQ <fleporcq@gmail.com>
2016-04-14 16:16:43 +0200

    feat(style): add sb-admin-2 bootstrap theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,47 @@ module.exports = function(grunt) {
         concat: {
             //...
         },
-        less: {
-            development: {
-                options: {
-                  compress: true,  //minifying the result
-                },
-                files: {
-                  //compiling dashboard.less into dashboard.css
-                  "./web/assets/stylesheets/dashboard.css": "./app/Resources/assets/stylesheets/dashboard.less"
-                }
+        copy:{
+            dashboard:{
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            './bower_components/bootstrap/dist/css/bootstrap.min.css',
+                            './bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css',
+                            './bower_components/font-awesome/css/font-awesome.min.css',
+                            './bower_components/metisMenu/dist/metisMenu.min.css',
+                        ],
+                        dest: './web/assets/stylesheets/',
+                        filter: 'isFile',
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            './bower_components/jquery/dist/jquery.min.js',
+                            './bower_components/bootstrap/dist/js/bootstrap.min.js',
+                            './bower_components/metisMenu/dist/metisMenu.min.js',
+                            './bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js',
+                        ],
+                        dest: './web/assets/javascripts/',
+                        filter: 'isFile',
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            './bower_components/font-awesome/fonts/**',
+                        ],
+                        dest: './web/assets/fonts/',
+                        filter: 'isFile',
+                    }
+                ]
             }
+        },
+        less: {
+            //...
         },
         uglify: {
             //...
@@ -32,8 +63,9 @@ module.exports = function(grunt) {
 
     // Plugin loading
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Task definition
